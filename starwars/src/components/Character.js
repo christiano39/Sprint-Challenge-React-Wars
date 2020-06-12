b6fa76ed4f7ffc134bb8b97370c8ee8264caa467
christiano39 <carneson39@gmail.com>
2020-06-12 09:40:47 -0600

    added CharacterContainer styled component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import CharacterContainer from '../styledComponents/CharacterContainer'
 
 export default function Character(props) {
     const { name, height, mass } = props
 
     return (
-        <div className='character-container'>
+        <CharacterContainer>
             <h2>{name}</h2>
             <p>Height: {height}</p>
             <p>Mass: {mass}</p>
-        </div>
+        </CharacterContainer>
     )
 }
---
+++ starwars/src/styledComponents/CharacterContainer.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export default styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: white;
+    width: 400px;
+    margin: 20px auto;
+    border: 3px solid black;
+`
