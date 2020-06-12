c774c6b131a92315a8035dda8ecd405221f59b25
christiano39 <carneson39@gmail.com>
2020-06-12 09:33:34 -0600

    display characters and stats from axios call

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import Character from './components/Character'
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [charactersData, setCharactersData] = useState()
+  const [page, setPage] = useState(1)
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get(`https://swapi.py4e.com/api/people/?page=${page}`)
+      .then(res => {
+        console.log(res)
+        setCharactersData(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [ page ])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        charactersData ? charactersData.results.map(char => {
+          return <Character key={char.name} name={char.name} height={char.height} mass={char.mass}/>
+        })
+        : <h2>Whoops! Looks like there's no data to display :(</h2>
+      }
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,13 @@
-// Write your Character component here
+import React from 'react'
+
+export default function Character(props) {
+    const { name, height, mass } = props
+
+    return (
+        <div className='character-container'>
+            <h2>{name}</h2>
+            <p>Height: {height}</p>
+            <p>Mass: {mass}</p>
+        </div>
+    )
+}
